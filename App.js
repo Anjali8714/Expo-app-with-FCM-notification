b1627a9226f0682f5dd055083d8de524ec0d5ee4
Anjali8714 <anjalit9714@gmail.com>
2025-05-06 14:58:33 +0530

    add fcm push notification

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 import { WebView } from "react-native-webview"
+import messaging from '@react-native-firebase/messaging';
+
 
 const App = () => {
+
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getToken = async()=>{
+    const token = await messaging().getToken()
+    console.log('Token =',token)
+  }
+
+  useEffect(()=>{
+    requestUserPermission()
+    getToken()
+  },[]);
+  
   return (
     <SafeAreaView style={styles.container}>
       <WebView source={{ uri:'https://googleauthnextjs.netlify.app/'}} style={styles.webview} />
